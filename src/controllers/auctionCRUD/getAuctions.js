287d9b36f95fd072b3cdd6a889531f2e058b5176
samuelalejo18 <samuelalejomonsalve@gmail.com>
2025-05-04 17:01:43 -0500

    Método en el controlador de subastas, método get, traer subastas y traer subastas por id

---
+++ src/controllers/auctionCRUD/getAuctions.js
@@ -0,0 +1,34 @@
+const Auction = require("../../models/Auction.js"); // Importar el modelo de subasta
+
+//get para obtener los datos de la subasta
+
+const getAuctions = async (req, res) => {
+  try {
+    const auctions = await Auction.find();
+
+    res.json(auctions);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error a traer las subastas", error: error.message });
+  }
+};
+
+//get para obtener los datos de la subasta por id
+const getAuctionByID = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const auctionByID = await AuctionByID.findById(id);
+
+    if (!auctionByID) {
+      return res.status(404).json({ message: "Subasta no encontrada" });
+    }
+    res.json(auctionByID);
+  } catch (error) {
+    res.status(500).json({
+      message: "Error a traer  la subasta por ID",
+    });
+  }
+};
+
+module.exports = { getAuctions, getAuctionByID };
