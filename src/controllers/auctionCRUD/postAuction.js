7a44e9404af908b03569246a502840811faf580e
felipemadeit <felipemadeit@gmail.com>
2025-05-11 18:13:49 -0500

    Creación del postAuction

---
+++ src/controllers/auctionCRUD/postAuction.js
@@ -0,0 +1,42 @@
+// Import the auction model
+const auction = require("../../models/Auction.js");
+
+
+// Create a new auction
+const createAuction = async (req, res) => {
+
+    try {
+
+        const {artworkId, startDate, endDate, status} = req.body;
+        
+
+        if(!artworkId) {
+            return res.status(400).json({message: "Se requiere ID de la obra de arte"});
+        }
+
+        if (!startDate || !endDate) {
+            return res.status(400).json({message: "Se requieren fechas de inicio y fin"});
+        }
+
+
+        const newAuction = new Auction({
+            artworkId,
+            startDate, 
+            endDate, 
+            status: status || "Scheduled",
+            participants: [],
+            winner: null
+        });
+
+
+        const savedAuction = await newAuction.save();
+
+        res.status(201).json(savedAuction);
+    } catch (error) {
+        console.error("Error creating function", error);
+        res.status(500).json({
+            message: "Error al crear la subasta",
+            error: error.message
+        });
+    }
+};
