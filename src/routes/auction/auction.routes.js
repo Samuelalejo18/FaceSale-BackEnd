90dd07cfba2253420cbd8b910916a68f3a2ac644
felipemadeit <felipemadeit@gmail.com>
2025-05-11 21:58:08 -0500

    Documentación de rutas hecha

--- src/routes/auction/auction.routes.js
+++ src/routes/auction/auction.routes.js
@@ -4,6 +4,7 @@ const router = Router();
 // Import middleware
 const authRequired = require("../../middlewares/validateToken.js");
 
+// Import controllers
 const { addBid } = require("../../controllers/auctionCRUD/addBid.js");
 const { deleteAuction } = require("../../controllers/auctionCRUD/deleteAuction.js");
 const { finalizeAuction } = require("../../controllers/auctionCRUD/finalizeAuction.js");
@@ -11,25 +12,58 @@ const { getAuctions, getAuctionByID } = require("../../controllers/auctionCRUD/g
 const { createAuction } = require("../../controllers/auctionCRUD/postAuction.js");
 const { updateAuction } = require("../../controllers/auctionCRUD/putAuction.js");
 
-// Get routes
+// ---------------------------------------------------------------------------------
+// GET ROUTES
+// ---------------------------------------------------------------------------------
+
+// /api/auctions
+// Obtiene todas las subastas disponibles
 router.get("/", getAuctions);
+
+// /api/auctions/:id
+// Ejemplo: /api/auctions/6523ab4c8b32a1b4567890cd
+// Obtiene una subasta específica por su ID
 router.get("/:id", getAuctionByID);
 
+// ---------------------------------------------------------------------------------
+// POST ROUTES
+// ---------------------------------------------------------------------------------
 
-// Post route - Create new auction
+// /api/auctions
+// Crea una nueva subasta - Requiere autenticación
+// Body: { artworkId, startDate, endDate, status }
 router.post("/", authRequired, createAuction);
 
-// Put routes
+// ---------------------------------------------------------------------------------
+// PUT ROUTES
+// ---------------------------------------------------------------------------------
+
+// /api/auctions/:id
+// Ejemplo: /api/auctions/6523ab4c8b32a1b4567890cd
+// Actualiza una subasta existente - Requiere autenticación
+// Body: { artworkId?, startDate?, endDate?, status?, participants?, winner? }
 router.put("/:id", authRequired, updateAuction);
+
+// /api/auctions/:id/bid
+// Ejemplo: /api/auctions/6523ab4c8b32a1b4567890cd/bid
+// Añade una nueva puja a la subasta - Requiere autenticación
+// Body: {userId, bidAmount}
 router.put("/:id/bid", authRequired, addBid);
+
+// /api/auctions/:id/finalize
+// Ejemplo: /api/auctions/6523ab4c8b32a1b4567890cd/finalize
+// Finaliza una subasta y determina el ganador - Requiere autenticación
 router.put("/:id/finalize", authRequired, finalizeAuction);
 
-// Delete route
-router.put("/:id", authRequired, updateAuction);
-router.put("/:id/bid", authRequired, addBid);
-router.put("/:id/finalize", authRequired, finalizeAuction);
+// ---------------------------------------------------------------------------------
+// DELETE ROUTES
+// ---------------------------------------------------------------------------------
 
-// Delete route
+// /api/auctions/:id
+// Ejemplo: /api/auctions/6523ab4c8b32a1b4567890cd
+// Elimina una subasta por su ID - Requiere autenticación
 router.delete("/:id", authRequired, deleteAuction);
 
 module.exports = router;
+
+
