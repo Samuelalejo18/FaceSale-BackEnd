e8c38a06ebf35d382559a317b3bb1b18b749e015
samuelalejo18 <samuelalejomonsalve@gmail.com>
2025-05-16 13:09:22 -0500

    push

--- src/controllers/auth/login.controller.js
+++ src/controllers/auth/login.controller.js
@@ -1,6 +1,6 @@
 const User = require("../../models/User.js");
 const bcrypt = require("bcrypt");
-const {reconocimientoFacial}=require("../reconocimientoFacial/reconocimientoFacial.js");
+const { reconocimientoFacial } = require("../reconocimientoFacial/reconocimientoFacial.js");
 //Importar token
 
 const { createAccessToken } = require("../../libs/jwt.js");
